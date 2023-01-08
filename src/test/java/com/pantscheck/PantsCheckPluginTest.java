f9cd72fe551e8d89c6ede287c9ca240ad61cc10c
Jack <jackwdurham@yahoo.co.uk>
2023-01-08 12:54:27 -0600

    test commit and push

--- src/test/java/com/pantscheck/PantsCheckPluginTest.java
+++ src/test/java/com/pantscheck/PantsCheckPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.pantscheck;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class PantsCheckPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(PantsCheckPlugin.class);
 		RuneLite.main(args);
 	}
 }
