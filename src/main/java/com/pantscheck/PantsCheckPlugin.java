66e6e0f4d0dbc9f52a27a9f57facf0f3511deca0
Jack <jackwdurham@yahoo.co.uk>
2023-01-08 13:55:36 -0600

    commit item pull changes

--- src/main/java/com/pantscheck/PantsCheckPlugin.java
+++ src/main/java/com/pantscheck/PantsCheckPlugin.java
@@ -3,10 +3,10 @@ package com.pantscheck;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.Client;
-import net.runelite.api.GameState;
+import net.runelite.api.*;
 import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.ItemContainerChanged;
+import net.runelite.api.events.OverheadTextChanged;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
@@ -37,12 +37,28 @@ public class PantsCheckPlugin extends Plugin
 	}
 
 	@Subscribe
+	public void onOverheadTextChanged(OverheadTextChanged e) {
+		if(e.getActor().equals(client.getLocalPlayer()) && e.getOverheadText().equals("Pants?")) {
+			client.getLocalPlayer().setOverheadText("Yep, I got pants on!");
+		}
+	}
+
+	@Subscribe
 	public void onGameStateChanged(GameStateChanged gameStateChanged)
 	{
 		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
 		{
 			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
 		}
+	}
+	//https://static.runelite.net/api/runelite-api/net/runelite/api/PlayerComposition.html
+	//https://github.com/runelite/runelite/issues/7954
+	@Subscribe
+	public void onItemContainerChanged(ItemContainerChanged event)
+	{
+		final ItemContainer container = event.getItemContainer();
+		final Item[] inv = container.getItems();
+		client.getLocalPlayer().setOverheadText(inv.toString());
 	}
 
 	@Provides
